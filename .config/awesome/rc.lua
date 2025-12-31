e8c6868fedd69b07d8f545aad66a134319469b0f
averevki <sandyverevkin@gmail.com>
2025-12-31 11:18:01 +0100

    awesomewm: return battery usage stat
    Signed-off-by: averevki <sandyverevkin@gmail.com>

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -218,14 +218,14 @@ awful.screen.connect_for_each_screen(function(s)
             wibox.widget.systray(),
 	    mykeyboardlayout,
 	    wibox.widget.textbox("| "),
-	    --awful.widget.watch("cat /sys/class/power_supply/BAT0/capacity", 60, function(widget, stdout)
-	--	    local battery = tonumber(stdout:sub(1, -2))  -- remove newline from stdout
-	--	    if battery <= 20 then
-	--		    widget:set_markup("<span foreground='#D22B2B'>" .. battery ..  "%</span> |")
-	--	    else
-	--		    widget:set_markup(battery .. "% |")
-	--	    end
-	  --  end),
+	    awful.widget.watch("cat /sys/class/power_supply/BAT0/capacity", 60, function(widget, stdout)
+		    local battery = tonumber(stdout:sub(1, -2))  -- remove newline from stdout
+		    if battery <= 20 then
+			    widget:set_markup("<span foreground='#D22B2B'>" .. battery ..  "%</span> |")
+		    else
+			    widget:set_markup(battery .. "% |")
+		    end
+	    end),
 	    mytextclock,
             s.mylayoutbox,
         },
